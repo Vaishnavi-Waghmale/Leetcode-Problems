1e53ff4e8860cf556e41fc25a96287176fdf572a
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-08-31 12:18:46 +0530

    Comments

--- Easy Problems/ComplementOfBase10Int.cpp
+++ Easy Problems/ComplementOfBase10Int.cpp
@@ -3,14 +3,14 @@ public:
     int bitwiseComplement(int n) {
         int m=n;
         int mask=0;
-        if(n==0){
+        if(n==0){ //Edge case
             return 1;
         }
         while(m!=0){
-            mask = (mask<<1) | 1;
+            mask = (mask<<1) | 1;  
             m = m>>1;
         }
-        int ans = (~n) & mask;
+        int ans = (~n) & mask;  
         
         return ans;
     }
