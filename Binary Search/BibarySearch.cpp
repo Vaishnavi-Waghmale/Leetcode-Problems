f4f15832a15369902b5c1701f683c1ad09dbd0f9
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-09-18 22:13:12 +0530

    New problem

---
+++ Binary Search/BibarySearch.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+
+    int mid = (start + end) / 2;
+
+    while (start <= end)
+    {
+
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        // going to right start= mid+1
+        if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        // going to left  end = mid-1
+        else
+        {
+            end = mid - 1;
+        }
+        mid = (start + end) / 2;
+    }
+    return -1;
+}
+
+int main()
+{
+
+    int even[6] = {2, 4, 6, 8, 12, 18};
+    int odd[5] = {3, 8, 11, 14, 16};
+
+    int index = binarySearch(odd, 5, 8);
+
+    cout << index;
+
+    return 0;
+}
