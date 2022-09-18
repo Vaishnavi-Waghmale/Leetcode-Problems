99f46a2f1557cb21b303938a560825548350fe48
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-09-18 22:23:16 +0530

    Improved from getting error

--- Binary Search/BibarySearch.cpp
+++ Binary Search/BibarySearch.cpp
@@ -6,7 +6,9 @@ int binarySearch(int arr[], int size, int key)
     int start = 0;
     int end = size - 1;
 
-    int mid = (start + end) / 2;
+    //int mid = (start + end) / 2;
+    
+    int mid = start + ((end-start)/2)
 
     while (start <= end)
     {
@@ -25,7 +27,9 @@ int binarySearch(int arr[], int size, int key)
         {
             end = mid - 1;
         }
-        mid = (start + end) / 2;
+        //mid = (start + end) / 2;  this can give error if our start and end both values are  2^31-1 hence use this
+        
+        mid = start + ((end-start)/2)
     }
     return -1;
 }
